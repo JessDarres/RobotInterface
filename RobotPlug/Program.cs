9609ad1054b3171581d35f6bd4a7de7fcebe54c7
jess <jessd@metamation.com>
2025-01-20 18:00:17 +0530

    Case 37033: Added Robot plugin and Robotserver tools for robot interface

---
+++ RobotPlug/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.IO.Pipes;
+using System.Text;
+using Petrel;
+
+namespace RobotPlug {
+   [Brick]
+   internal class Program : IInitializable {
+      public static void Main () {
+      }
+
+      public void Initialize () {
+         lock (sLock) {
+            Console.WriteLine ("Initialize RAPlug");
+            Brick.DisableBrickMechanism = false;
+            Brick.Verbose = true;
+            while (mRAInvoker == null) {
+               var inv = Brick.GetAll<IRightAngleInvoker> ().ToList ();
+               if (inv != null) mRAInvoker = inv[0];
+            }
+            var thr = new Thread (ListenForMessages);
+            thr.Start ();
+         }
+      }
+
+      void ListenForMessages () {
+         while (true) {
+            using var client = new NamedPipeClientStream (".", PipeName, PipeDirection.InOut);
+            try {
+               client.Connect ();
+               byte[] buffer = new byte[256];
+               int bytesRead;
+               bytesRead = client.Read (buffer, 0, buffer.Length);
+               if (bytesRead > 0) {
+                  string message = Encoding.UTF8.GetString (buffer, 0, bytesRead);
+                  Console.WriteLine ("Received from pipe: " + message);
+                  var msgs = message.Split (',');
+                  var ret = false;
+                  if (mRAInvoker != null) {
+                     switch (msgs[0].ToLower ()) {
+                        case "gohome":
+                           int.TryParse (msgs[1], out int mode);
+                           ret = mRAInvoker.GoHome (mode);
+                           break;
+                        case "runprogram":
+                           ret = mRAInvoker.RunProgram (msgs[1]);
+                           break;
+                        default:
+                           break;
+                     }
+                  }
+                  var messageBytes = Encoding.UTF8.GetBytes (ret ? "200" : "400");
+                  client.Write (messageBytes);
+                  client.WaitForPipeDrain ();
+                  Console.WriteLine ($"Client: cleint sent return code \"{ret}\" sucessfully");
+               }
+            } catch (TimeoutException) {
+               Console.WriteLine ("Timeout occurred while connecting to pipe.");
+            } catch (IOException ex) {
+               Console.WriteLine ("Error: " + ex.Message);
+            }
+         }
+      }
+
+      public void Uninitialize () => Console.WriteLine ("UnInitialize RAPlug!");
+
+      IRightAngleInvoker? mRAInvoker;
+      static readonly object sLock = new ();
+      const string PipeName = "RAPIPE";
+   }
+}
---
+++ RobotServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Petrel;
+using System.IO.Pipes;
+using System.Text;
+
+namespace RobotServer {
+   internal class Program {
+      static void Main (string[] args) {
+         var builder = WebApplication.CreateBuilder (args);
+         builder.WebHost.UseUrls ("http://localhost:9011");
+         //builder.Services.AddControllers ();
+         builder.Services.AddEndpointsApiExplorer ();
+         builder.Services.AddSwaggerGen ();
+         var app = builder.Build ();
+         //app.UseRouting ();
+         // Configure the HTTP request pipeline.
+         ////if (app.Environment.IsDevelopment ()) {
+            app.UseSwagger ();
+            app.UseSwaggerUI ();
+        //// }
+         app.UseHttpsRedirection ();
+         //app.MapControllers ();
+         app.MapPost ("GoHome", async () => {
+            var ret = await Task.Run (() => SendMessage ("GoHome"));
+            if (ret == "200") return Results.Ok ();
+            else return Results.BadRequest ();
+         });
+         app.MapPost ("GoHome/{mode}", async (string mode) => {
+            var ret = await Task.Run (() => SendMessage ("GoHome", mode));
+            if (ret == "200") return Results.Ok ();
+            else return Results.BadRequest ();
+         });
+
+         app.MapPost ("RunProgram/{progName}", async (string progName) => {
+            var ret = await Task.Run (() => SendMessage ("RunProgram", progName));
+            if (ret == "200") return Results.Ok ();
+            else return Results.BadRequest ();
+         });
+
+         app.Run ();
+
+      }
+
+      static string SendMessage (string message, string param = "") {
+         var ret = "400";
+         using var stm = new NamedPipeServerStream (PipeName, PipeDirection.InOut);
+         try {
+            stm.WaitForConnection ();
+            var sendMsg = string.Join (',', [message, param]);
+            var msgBytes = Encoding.UTF8.GetBytes (sendMsg);
+            stm.Write (msgBytes);
+            stm.WaitForPipeDrain ();
+            byte[] buffer = new byte[256];
+            int bytesRead;
+            bytesRead = stm.Read (buffer, 0, buffer.Length);
+            if (bytesRead > 0) {
+               ret = Encoding.UTF8.GetString (buffer, 0, bytesRead);
+               Console.WriteLine ("Server : messages read from client: " + ret);
+            }
+         } catch (Exception ex) {
+            return ret;
+         }
+         return ret;
+      }
+
+      const string PipeName = "RAPIPE";
+   }
+}
