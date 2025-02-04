7d1c20d02757dfc2aecfc555c93be67a0393c292
jess <jessd@metamation.com>
2025-02-04 18:29:11 +0530

    Removed goto mode option from gohom

--- RobotClient/Program.cs
+++ RobotClient/Program.cs
@@ -8,23 +8,25 @@ namespace RobotClient {
          client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/json"));
          for (; ; ) {
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine ("gohome -> Goto the entered mode in home page");
+            //Console.WriteLine ("gohome -> Goto the entered mode in home page");
+            Console.WriteLine ("gohome -> Goto home page");
             Console.WriteLine ("runprogram -> Run program");
             Console.WriteLine ("1 -> Goto home page");
             Console.WriteLine ("2 -> Run p8 program");
             var input = Console.ReadLine ();
             var message = "http://localhost:9011/";
             switch (input?.ToLower ()) {
-               case "gohome":
-                  Console.WriteLine ("Please enter the mode number :");
-                  var modeNum = Console.ReadLine ();
-                  message += $"GoHome/{modeNum}";
-                  break;
+               //case "gohome":
+               //   Console.WriteLine ("Please enter the mode number :");
+               //   var modeNum = Console.ReadLine ();
+               //   message += $"GoHome/{modeNum}";
+               //   break;
                case "runprogram":
                   Console.WriteLine ("Please enter the program name :");
                   var name = Console.ReadLine ();
                   message += $"RunProgram/{name}";
                   break;
+               case "gohome":
                case "1":
                   message += "GoHome/0";
                   break;
